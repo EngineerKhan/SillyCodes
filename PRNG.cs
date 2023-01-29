881575127dbcceeaa86715c3b7809a5210dad4f0
Talha <engineer.khan@live.com>
2023-01-29 12:03:42 +0500

    Update PRNG.cs

--- PRNG.cs
+++ PRNG.cs
@@ -2,22 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class main 
+public class RandomGenerator
 {
-    public static int SimplePRNG(int seed)
+    static int seed;
+    public RandomGenerator(int s)
     {
+        seed = s;
+    }
+
+    public int PRNG()
+    {
+        
         int x = 1069;
         int y = 2423;
-        int z = ((x*seed+y)%y)+((x*seed+y)/y); //Because I can!
+        int z = ((x*seed+y)%y)+((x*seed+y)/y);
+
+        seed++;
+
         return z;
     }
-    
+}
+
+public class main 
+{
+     
     public static void Main()
     {
+        RandomGenerator rng = new RandomGenerator(3);
         int[] values = Enumerable.Range(1, 100).ToArray();
         foreach(int n in values)
         {
-            Console.WriteLine(SimplePRNG(n));
+            Console.WriteLine(rng.PRNG());
         }
         
     }
